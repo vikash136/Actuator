668d03206be33f4588e72da9fe30b9e362c9a191
vikash soni <sonivikash136@gmail.com>
2022-12-16 11:00:30 +0530

    first

--- actuator-example/src/main/java/com/learn/controller/HomeController.java
+++ actuator-example/src/main/java/com/learn/controller/HomeController.java
@@ -1,2 +1,19 @@
-package com.learn.controller;public class HomeController {
+package com.learn.controller;
+
+import com.learn.helpler.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HomeController {
+
+    @Autowired
+    private Student student;
+    @GetMapping("/getdata")
+    public Map<String,String>getdata(){
+        return Map.of("name","Vikash Soni");
+    }
 }
